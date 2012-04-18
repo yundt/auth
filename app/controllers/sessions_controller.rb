f6a80555c77f9da0bb8cb9a25aa3c9c469eec75d
Marek Nowak <mareknowakk@gmail.com>
2012-04-18 20:46:08 +0200

    requiring password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,18 @@ class SessionsController < ApplicationController
   helper_method :logged_in?
 
   def create
-    session[:logged_in] = true
-    redirect_to session_path
+    if params[:password] == "SuperSecret" # SHA1 it or whatever
+      session[:logged_in] = true
+
+      redirect_to session_path, :notice => 'just logged in'
+    else
+      redirect_to session_path, :alert => 'not really'
+    end
   end
 
   def destroy
     session.delete :logged_in
-    redirect_to session_path
+    redirect_to session_path, :notice => 'logged out'
   end
 
   private
